8820291ac3b08e3a0327ef4c90e2f332cb16744c
Joe <joe.ekiert@gmail.com>
2015-08-10 19:54:04 -0700

    creating api index view

--- routes/apiIndex.js
+++ routes/apiIndex.js
@@ -2,7 +2,7 @@ var express = require('express'),
     router = express.Router();
 
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Welcome to TeamPicker API version 1.0' });
+  res.render('apiIndex', { title: 'Welcome to TeamPicker API version 1.0' });
 });
 
 module.exports = router;
