5e99970795d1646dc98967f440f21270707caa61
Joe <joe.ekiert@gmail.com>
2015-08-10 21:39:34 -0700

    Cleaning out app.js
    - removing unneeded routes
    - adding comment formatting
    - building out alternative routes definitionings

--- app.js
+++ app.js
@@ -4,42 +4,50 @@ var favicon = require('serve-favicon');  // TODO JOE: get rid of this
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// ================
 // TURN ON ZE APP
+// ===========
 var app = express();
 
 // define logging service
 var logger = require('morgan');
 
+// ================
 // define routes
-var indexRoute = require('./routes/index');
+// ===========
+var routes = require('./routes/routes');
 app.use('/', indexRoute);
-var apiIndexRoute = require('./routes/apiIndex');
+var api = require('./routes/api');
 app.use('/api/v1', apiIndexRoute);
-var playerRoute = require('./routes/player');
-app.use('/api/v1/player', playerRoute);
-var teamRoute = require('./routes/team');
-app.use('/api/v1/team', teamRoute);
 
+// ====================
 // view engine setup
+// ===============
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// =======================================================
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+// app.use(favicon(__dirname + '/public/favicon.ico'));
+// ===========================================
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// =========================================
 // catch 404 and forward to error handler
+// =================================
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
+// ================
 // error handlers
+// ===========
 
 // development error handler
 // will print stacktrace
