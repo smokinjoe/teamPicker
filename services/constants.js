00d54b524d9e30a807d84f716706f7fc8cbae51b
Joe <joe.ekiert@gmail.com>
2015-08-12 22:33:45 -0700

    altering office players and removing test stuff

--- services/constants.js
+++ services/constants.js
@@ -1,5 +1,5 @@
 var constants = {
-	WHOLE_OFFICE: [ 'Joe', 'Manny', 'Michael', 'Vengadesh', 'Pankaj', 'Sunil', 'Max', 'Roman', 'Audrey', 'Sohail', 'Jason', 'Steve', 'Josh', 'Scott', 'Stephen', 'Bobby' ]
+	WHOLE_OFFICE: [ 'Joe', 'Manny', 'Michael', 'Vengadesh', 'Pankaj', 'Sunil', 'Max', 'Roman', 'Audrey', 'Sohail', 'Scott', 'Stephen', 'Bobby' ]
 };
 
 module.exports = constants;
