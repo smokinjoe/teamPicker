4c48991e5bcb1fec9ce25c906159b7fe8ea94dcf
Joe <joe.ekiert@gmail.com>
2015-08-18 22:20:38 -0700

    getter/setter routes
    - dammit I added the wrong file to last commit, i'm removing the association of a player to a team, only having team to a player for now

--- routes/team.js
+++ routes/team.js
@@ -1,5 +1,14 @@
+var Player = require('../models/player'),
+    Team = require('../models/team');
+
 var methods = {
+  index: function (req, res, next) {
+    res.send('respond with resource');
+  },
   get: function (req, res, next) {
+    res.send('respond with resource');
+  },
+  create: function (req, res, next) {
     res.send('respond with resource');
   }
 };
