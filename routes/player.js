e8a7ea4047a91e4b21e2d19eef478ef8dd9661c3
Joe <joe.ekiert@gmail.com>
2015-08-18 22:28:33 -0700

    Trying to fill out the stubbed methods

--- routes/player.js
+++ routes/player.js
@@ -3,13 +3,44 @@ var Player = require('../models/player'),
 
 var methods = {
   index: function (req, res, next) {
-    res.send('respond with resource');
+    res.json({
+      players: Player.getAll()
+    });
   },
   get: function (req, res, next) {
-    res.send('respond with resource');
+    var id = req.param('id'),
+        player;
+
+    Player.findOne({ id: id }, function (err, player) {
+      if (err) {
+        console.log('Error: ', err);
+        throw err
+      }
+      if (player) {
+        console.log('Found player: ', username);
+        res.json({
+          player: player
+        });
+      }
+      else {
+        res.status(404);
+      }
+    });
   },
   create: function (req, res, next) {
-    res.send('respond with resource');
+    var newPlayer = new Player();
+    newPlayer.name = req.param('name');
+
+    newPlayer.save(function (err) {
+      if (err) {
+        console.log('Error in saving player: ', err);
+        throw err;
+      }
+      console.log('Player created successfully');
+      res.json({
+        player: newPlayer
+      });
+    });
   }
 };
 
