75bc154306af6304f6f15acf2fd7e1a1cc3fc07b
Joe <joe.ekiert@gmail.com>
2015-08-20 21:51:27 -0700

    Setting up global filters by using Vue to define them

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,21 @@
+Vue.filter('upper', function (value) {
+  return value.toUpperCase();
+});
+
+Vue.filter('lower', function (value) {
+  return value.toLowerCase();
+})
+
 var viewModel = new Vue({
   el: '#my-vue-instance',
   data: {
     string: 'Custom Filters'
-  },
-  methods:{
-  },
-  filters: {
-    upper: function (value) {
-      return value.toUpperCase();
-    },
-    lower: function (value) {
-      return value.toLowerCase();
-    }
+  }
+});
+
+var secondViewModel = new Vue({
+  el: '#another-vue-instance',
+  data: {
+    string: 'Lorem ipsum dolor sit amet'
   }
 });
