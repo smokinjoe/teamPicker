5289565724a3e18ef37b7d1628117ac1f5710cfc
Joe <joe.ekiert@gmail.com>
2015-08-20 21:29:44 -0700

    Mastering 2-way binding as well as v-on, v-model and v-repeat

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,16 @@
 var viewModel = new Vue({
   el: '#my-vue-instance',
   data: {
-    textInput: 'Too hot, hot damn.'
+    textInput: 'Too hot, hot damn.',
+    libraries: ['angular.js', 'node.js', 'mithril.js', 'express.js']
+  },
+  methods: {
+    addLibrary: function () {
+      this.libraries.push(this.newLibrary);
+      this.newLibrary = '';
+    },
+    deleteLibraries: function () {
+      this.libraries = [];
+    }
   }
 });
