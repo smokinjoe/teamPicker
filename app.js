4edf3e514c14943f5c29fdbf69f3c285a7ff55d6
Joe <joe.ekiert@gmail.com>
2015-08-23 21:35:04 -0700

    Adding chris and removing vue route

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ var morgan = require('morgan');
 // ===========
 var routes = require('./routes/routes');
 app.get('/', routes.index);
-app.get('/vue', routes.vue);
 app.get('/partials/:name', routes.partials)
 var api = require('./routes/api');
 app.get('/api/v1', api.index)
