436ed0fb7330154b50def8fdaa6f7b5618af700c
Joe <joe.ekiert@gmail.com>
2015-11-02 02:40:23 +0000

    trying to use ui-router

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,46 @@
 (function () {
   "use strict";
 
-  var dependencies = ['ngRoute',
-                      'App.constants', 'App.services', 'App.controllers'];
+  angular.module('App.states', []).
+  config(['$stateProvider', '$urlRouteProvider', function ($stateProvider, $urlRouteProvider) {
+    $urlRouteProvider.otherwise('/');
 
-  var app = angular.module('teamPickerApp', dependencies);
-
-  app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-  	$routeProvider.
-      when('/', {
+    $stateProvider.
+      state('root', {
+        url: '/',
         templateUrl: 'partials/teamSelect',
         controller: 'TeamSelectCtrl'
       }).
-      when('/teamName', {
+      state('team', {
+        url: '/teamName',
         templateUrl: 'partials/teamName',
         controller: 'TeamNameCtrl'
-      }).
-      otherwise({
-        redirectTo: '/'
       });
-
-    $locationProvider.html5Mode(true);
   }]);
+
+  var dependencies = ['ngRoute',
+                      'App.constants', 'App.services', 'App.controllers', 'App.states'];
+
+  var app = angular.module('teamPickerApp', dependencies);
+
+
+
+
+//   app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+//   	$routeProvider.
+//       when('/', {
+//         templateUrl: 'partials/teamSelect',
+//         controller: 'TeamSelectCtrl'
+//       }).
+//       when('/teamName', {
+//         templateUrl: 'partials/teamName',
+//         controller: 'TeamNameCtrl'
+//       }).
+//       otherwise({
+//         redirectTo: '/'
+//       });
+
+//     $locationProvider.html5Mode(true);
+//   }]);
 
 }());
